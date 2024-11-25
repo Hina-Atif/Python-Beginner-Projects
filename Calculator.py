e66ec41484e981ab11a57a5e569412c9cb84a555
hina atif <hinaatif355@gmail.com>
2024-11-25 18:59:50 +0500

    Added Calculator project

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+# Taking two numbers as input
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+# Converting inputs to numbers
+num1 = float(num1)  # Convert to a decimal number
+num2 = float(num2)
+
+# Performing calculations
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
